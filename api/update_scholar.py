0ac3bad48cad75dd7f8a7039aff6c28ab40a2424
suyongyi <503553364@qq.com>
2024-10-13 17:01:35 +0800

    upload

---
+++ api/update_scholar.py
@@ -0,0 +1,28 @@
+from http.server import BaseHTTPRequestHandler
+import os
+
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+
+        with open("./scholar/scholar_results.json", 'rb') as f:
+            for line in f.readlines():
+                self.wfile.write(line)
+                self.wfile.write(b"\n")
+        # self.wfile.write('Hello, world test!'.encode('utf-8'))
+        # if not os.path.exists('test.txt'):
+        #     with open('test.txt', 'w') as f:
+        #         f.write('abcded')
+        #     self.wfile.write('create file!\n'.encode('utf-8'))
+        
+        # with open('test.txt', 'rb') as f:
+        #     self.wfile.write(f.readline())
+        # print(os.path.abspath('./'))
+        # print(os.path.join(os.path.dirname(__file__)))
+        # with open(os.path.join(os.path.dirname(__file__), 'static_file.txt'), 'rb') as f:
+        #     self.wfile.write(f.readline())
+        return
