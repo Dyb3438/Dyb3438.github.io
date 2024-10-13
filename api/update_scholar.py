bf02e95c96a1bf7ef6ae5f4cd26441896bcc290d
suyongyi <503553364@qq.com>
2024-10-13 17:04:30 +0800

    upload

--- api/update_scholar.py
+++ api/update_scholar.py
@@ -8,7 +8,7 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-
+        self.wfile.write(b"hello world")
         with open("./scholar/scholar_results.json", 'rb') as f:
             for line in f.readlines():
                 self.wfile.write(line)
