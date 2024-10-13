1f2c841b9e7f6e6ac61c994f6aa5e72d87ce76d1
suyongyi <503553364@qq.com>
2024-10-13 17:10:42 +0800

    upload

--- api/update_scholar.py
+++ api/update_scholar.py
@@ -10,7 +10,12 @@ class handler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b"hello world" + b'\n')
         self.wfile.write((os.path.join(os.path.dirname(__file__))).encode('utf-8') + b'\n')
-        with open("./scholar/scholar_results.json", 'rb') as f:
+        with open(os.path.join(os.path.dirname(__file__), '../', "./scholar/scholar_results.json"), 'rb') as f:
+            for line in f.readlines():
+                self.wfile.write(line)
+                self.wfile.write(b"\n")
+        
+        with open(os.path.join(os.path.dirname(__file__), '../', "./public/scholar/scholar_results.json"), 'rb') as f:
             for line in f.readlines():
                 self.wfile.write(line)
                 self.wfile.write(b"\n")
