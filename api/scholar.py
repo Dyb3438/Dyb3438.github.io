b0693afe8a88c01ffdb119bff42060a9af3d58a7
suyongyi <503553364@qq.com>
2024-10-14 13:03:12 +0800

    upload

--- api/scholar.py
+++ api/scholar.py
@@ -9,12 +9,12 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write(b"hello world" + b'\n')
-        self.wfile.write((os.path.join(os.path.dirname(__file__))).encode('utf-8') + b'\n')
-
+        
         blobs = blob_store.list({
             'limit': '5',
         })
 
         self.wfile.write(json.dumps(blobs).encode('utf-8'))
         return
+    
+    
